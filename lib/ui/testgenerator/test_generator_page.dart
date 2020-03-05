da70110fea8a845000599f6bd1b6b276c380e8c5
mind <skyphantomhz@gmail.com>
2020-03-05 21:24:19 +0700

    Update generate test screen

--- lib/ui/testgenerator/test_generator_page.dart
+++ lib/ui/testgenerator/test_generator_page.dart
@@ -37,48 +37,77 @@ class _TestGeneratorPageState extends State<TestGeneratorPage> {
         leading: BackButton(),
       ),
       body: SafeArea(
-        child: StreamBuilder<List<Zhistory>>(
-          stream: testGeneratorBloc.histories,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            List<Zhistory> data = snapshot.data;
-            if (data == null || data.length == 0) {
-              return Container(
-                child: Center(child: Text("Lịch sử trống")),
-              );
-            } else {
-              return ListView.builder(
-                  itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    Zhistory history = data[index];
-                    final leadIcon = history.result == 1
-                        ? Icon(
-                            FontAwesomeIcons.solidCheckCircle,
-                            color: Colors.green,
-                          )
-                        : Icon(
-                            FontAwesomeIcons.solidTimesCircle,
-                            color: Colors.red,
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(50),
+              child: RaisedButton.icon(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {
+                  testGeneratorBloc.generateQuestions();
+                },
+                icon: Icon(
+                  FontAwesomeIcons.plus,
+                  color: Colors.white,
+                ),
+                label: Text(
+                  "Làm bài kiểm tra",
+                  style: Theme.of(context).textTheme.button,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  Container(
+                      margin: EdgeInsets.only(left: 20),
+                      child: Text(
+                        "Lịch sử thi:",
+                        style: Theme.of(context).textTheme.title,
+                      )),
+                  Expanded(
+                    child: StreamBuilder<List<Zhistory>>(
+                      stream: testGeneratorBloc.histories,
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        List<Zhistory> data = snapshot.data;
+                        if (data == null || data.length == 0) {
+                          return Container(
+                            child: Center(child: Text("Lịch sử trống")),
                           );
-                    return Card(
-                        child: ListTile(
-                      leading: leadIcon,
-                      title: Text(
-                          "Result: ${history.numberTrue}/${history.numberTrue + history.numberOfFalse}"),
-                      trailing: Text(
-                          convertTimeDuration((history.time * 60).round())),
-                    ));
-                  });
-            }
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          testGeneratorBloc.generateQuestions();
-        },
-        child: Icon(
-          FontAwesomeIcons.plus,
-          color: Colors.white,
+                        } else {
+                          return ListView.builder(
+                              itemCount: data.length,
+                              itemBuilder: (context, index) {
+                                Zhistory history = data[index];
+                                final leadIcon = history.result == 1
+                                    ? Icon(
+                                        FontAwesomeIcons.solidCheckCircle,
+                                        color: Colors.green,
+                                      )
+                                    : Icon(
+                                        FontAwesomeIcons.solidTimesCircle,
+                                        color: Colors.red,
+                                      );
+                                return Card(
+                                    child: ListTile(
+                                  leading: leadIcon,
+                                  title: Text(
+                                      "Result: ${history.numberTrue}/${history.numberTrue + history.numberOfFalse}"),
+                                  trailing: Text(convertTimeDuration(
+                                      (history.time * 60).round())),
+                                ));
+                              });
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
